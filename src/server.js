49ba335f7def371747b74bee6aee91ae9c5f4de2
hikio-17 <fajri010217@gmail.com>
2023-01-21 02:41:14 -0800

    create CRUD  api for albums

--- src/server.js
+++ src/server.js
@@ -1,12 +1,24 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const albums = require('./api/albums');
 
 const init = async () => {
   const server = Hapi.server({
     host: process.env.HOST,
     port: process.env.PORT,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
+
+  await server.register([
+    {
+      plugin: albums,
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
