9cdd736ca0e0e0eaa5328f4f56461946d2891878
hikio-17 <fajri010217@gmail.com>
2023-01-21 02:14:39 -0800

    setup server

---
+++ src/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    host: process.env.HOST,
+    port: process.env.PORT,
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
